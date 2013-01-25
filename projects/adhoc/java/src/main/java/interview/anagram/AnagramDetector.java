ca926f43a3523c297d464bc285ef8d34ea5bb750
Tom Gagnier <tomgagnier@gmail.com>
2013-01-25 17:45:16 -0500

    added anagram detector

---
+++ projects/adhoc/java/src/main/java/interview/anagram/AnagramDetector.java
@@ -0,0 +1,20 @@
+package interview.anagram;
+
+import java.util.regex.*;
+
+public class AnagramDetector {
+
+  private static final Pattern NON_ALPHABET = Pattern.compile("[^A-Za-z]");
+
+  public static boolean areAnagrams(String s1, String s2) {
+    return censusFor(s1).equals(censusFor(s2));
+  }
+
+  private static CharacterCensus censusFor(String s) {
+    return new CharacterCensus().add(stripNonAlphabet(s).toLowerCase());
+  }
+
+  private static String stripNonAlphabet(String s) {
+    return NON_ALPHABET.matcher(s == null ? "" : s).replaceAll("");
+  }
+}
